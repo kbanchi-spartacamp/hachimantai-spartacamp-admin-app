c2cc9639c75dc72f1a279280431761fa69096ea1
kbanchi1111 <kbanchi.1111@gmail.com>
2021-10-02 14:03:01 +0900

    update common header and footer

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,12 +1,22 @@
 @section('header')
-    <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom shadow-sm">
-        <a href="/top" class="h5 my-0 me-md-auto fw-normal navbar-brand text-white">八幡平スパルタキャンプWeb 運営</a>
-        <nav class="my-2 my-md-0 me-md-3">
-            <span class="p-2 text-dark"></span>
-        </nav>
-        <form action="/logout" method="post">
-            @csrf
-            <input class="btn text-light" type="submit" value="ログアウト" />
-        </form>
-    </header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark mb-2">
+        <a class="navbar-brand logo text-white" href="/top">八幡平Web運営</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link text-light" href="/top">Home <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0" method="POST" action="{{ route('logout') }}">
+                @csrf
+                <span class="mr-sm-2 text-light">{{ Auth::user()->name }}</span>
+                <input class="btn btn-outline-danger my-2 my-sm-0 text-light" type="submit" value="{{ __('Logout') }}">
+            </form>
+        </div>
+    </nav>
 @endsection
